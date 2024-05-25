6da629ab74c29021b7b504159a0b91374a9c337e
hue ho <hthi53503@gmail.com>
2024-05-25 10:52:46 +0700

    finish exercise 5

--- exercise-5/src/data.js
+++ exercise-5/src/data.js
@@ -1,1 +1,47 @@
-/* Your data here */
+import h1 from "./assets/h1.png"
+import h2 from "./assets/h2.png"
+import h3 from "./assets/h3.png"
+import h4 from "./assets/h4.png"
+export const AVAILABLE_Cart = [
+  {
+    id: "p1",
+    name: "hue",
+    title: "cold",
+    description: "ao nay rat dep hay mac no",
+    image: {
+      src: h4,
+      alt: "A tranquil forestwith a cascading waterfall amidst greenery.",
+    },
+  },
+  {
+    id: "p2",
+    name: "huyen",
+    title: "cold",
+    description: "ao nay rat dep hay mac no",
+    image: {
+      src: h1,
+      alt: "A tranquil forestwith a cascading waterfall amidst greenery.",
+    },
+  },
+  {
+    id: "p3",
+    name: "thu",
+    title: "cold",
+    description: "ao nay rat dep hay mac no",
+    image: {
+      src: h2,
+      alt: "A tranquil forestwith a cascading waterfall amidst greenery.",
+    },
+  },
+  {
+    id: "p4",
+    name: "nama",
+    title: "cold",
+    description: "ao nay rat dep hay mac no",
+    image: {
+      src: h3,
+      alt: "A tranquil forestwith a cascading waterfall amidst greenery.",
+    },
+  },
+
+];
